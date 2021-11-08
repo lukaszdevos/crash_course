c8adccace667e2704a51814e841efc528cd96ac0
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-08 16:49:36 +0100

    [CC-404] docker with env variables

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM python:3.9
 RUN mkdir /app
 COPY ./requirements.txt /app/requirements.txt
 WORKDIR /app/backend
-USER app
 RUN pip install -r /app/requirements.txt
 
 EXPOSE 8000
--- backend/crashcourse/settings.py
+++ backend/crashcourse/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv("./.env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dm^esvv25+1+!jnjt7)6o%p*$ft7+uz%2m9f^v5#3lnv+mab*3'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -74,9 +77,12 @@ WSGI_APPLICATION = 'crashcourse.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": os.getenv("DB_HOST"),
+        "NAME": os.getenv("POSTGRES_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
