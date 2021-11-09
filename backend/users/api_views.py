e7935df1e52585598220cbfd21784025ee30f9a4
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-09 16:44:07 +0100

    [CC-405] hash password

--- backend/users/api_views.py
+++ backend/users/api_views.py
@@ -4,5 +4,4 @@ from users.serializers import UserSerializer
 
 
 class UserCreateView(CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from users.models import User
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,3 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "password",
         ]
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data.get("password"))
+        return super().create(validated_data)
+
