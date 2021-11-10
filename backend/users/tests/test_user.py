8e509dd0a2cde137159ee1769a1f00bde3267555
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-10 11:26:13 +0100

    [CC-405] ready test for creat user

--- backend/users/tests/test_user.py
+++ backend/users/tests/test_user.py
@@ -4,6 +4,8 @@ import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
 
+from users.tests.user_factories import UserDictFactory
+
 pytestmark = pytest.mark.django_db
 
 
@@ -12,10 +14,14 @@ class UserTest(TestCase):
         self.client = APIClient()
 
     def test_user_create(self):
-        data = {"email": "test1234@test.com", "password": "test"}
-        url = reverse("users:register")
-        self.given_post_response_endpoint(url, data)
+        self._given_user_has_been_created()
+
         self.assertEqual(self.response.status_code, 201)
+
+    def _given_user_has_been_created(self):
+        self.data = UserDictFactory.build()
+        url = reverse("users:register")
+        self.given_post_response_endpoint(url, self.data)
 
     def given_post_response_endpoint(self, url, data):
         self.response = self.client.post(url, data)
