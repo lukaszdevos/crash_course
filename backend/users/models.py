dd7faa7d4e888c25fb0b406c584fd04e79cd5e02
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-10 16:05:18 +0100

    [CC-406] user token model

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,3 +8,10 @@ class User(AbstractBaseUser):
     display_name = models.CharField(null=True, blank=True, max_length=128)
 
     USERNAME_FIELD = "email"
+
+
+class UserToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE())
+    token = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
