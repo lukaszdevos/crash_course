687155a9d83d1f62a8e1bed90a8d407d6f524ef1
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-10 13:11:10 +0100

    [CC-405] run linter

--- backend/users/migrations/0003_user_display_name.py
+++ backend/users/migrations/0003_user_display_name.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_user_is_active'),
+        ("users", "0002_user_is_active"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='display_name',
+            model_name="user",
+            name="display_name",
             field=models.CharField(blank=True, max_length=128, null=True),
         ),
     ]
