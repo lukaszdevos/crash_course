9e41a6f01a968f3600c419231cce128e432df961
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-12 16:08:23 +0100

    [CC-407] add jwt

--- backend/crashcourse/settings.py
+++ backend/crashcourse/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "users",
 ]
 
@@ -108,6 +109,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 
 # Internationalization
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from users import api_views
 
 app_name = "users"
@@ -7,4 +9,6 @@ app_name = "users"
 urlpatterns = [
     path("register/", api_views.UserCreateView.as_view(), name="register"),
     path("activate/", api_views.UserActivateView.as_view(), name="activate"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
