d1d724f7076957c59ed6790565945b25c420011b
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-15 21:10:54 +0100

    [CC-409] view set

--- backend/projects/api_views.py
+++ backend/projects/api_views.py
@@ -1,8 +1,10 @@
 from projects.models import Project
 from projects.serializers import ProjectSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
--- backend/projects/models.py
+++ backend/projects/models.py
@@ -6,5 +6,5 @@ class Project(models.Model):
     name = models.CharField(max_length=128, blank=False)
     member = models.ManyToManyField(User, blank=True, related_name="project")
     created_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="created_by"
+        User, on_delete=models.DO_NOTHING, related_name="created_by"
     )
