74581af9ac85b56b17fa9f486ba56d9bb46dbade
LukaszLaszuk <lukasz.laszuk@stxnext.pl>
2021-11-15 15:43:13 +0100

    [CC-409] project model

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    member = models.ManyToManyField(User, blank=True, related_name="project")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
